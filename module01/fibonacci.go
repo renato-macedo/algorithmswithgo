5886d894f318cf03d3faad8b5963d6b9419bd44c
Renato <renatomaceneto@gmail.com>
2020-01-31 23:21:46 -0300

    day 3

--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -25,5 +25,18 @@ package module01
 //   Fibonacci(14) => 377
 //
 func Fibonacci(n int) int {
-	return 0
+	if n <= 1 {
+		return n
+	}
+
+	nMinus2 := 0
+	nMinus1 := 1
+	var cur int
+
+	for i := 2; i <= n; i++ {
+		cur = nMinus1 + nMinus2
+		nMinus2 = nMinus1
+		nMinus1 = cur
+	}
+	return cur
 }
